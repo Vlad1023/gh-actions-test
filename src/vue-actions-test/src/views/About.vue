b8c813735ddbb61e0ff34029fb44d4862731a0d3
valerachuk <valerachuk@gmail.com>
2021-10-14 21:44:53 +0300

    #1: Added "Vue.js" definition to "About" page

--- src/vue-actions-test/src/views/About.vue
+++ src/vue-actions-test/src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Vue.js (commonly referred to as Vue; pronounced /vjuː/, like "view") is an open-source model–view–viewmodel front end JavaScript framework for building user interfaces and single-page applications. It was created by Evan You, and is maintained by him and the rest of the active core team members.</p>
   </div>
 </template>
